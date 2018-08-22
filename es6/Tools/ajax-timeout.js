93ea585754290a645fe43ace48b3bd0d400bb5c6
shanshan_pan <shanshan_pan@kingdee.com>
2018-08-22 11:18:27 +0800

    'overtime'

---
+++ es6/Tools/ajax-timeout.js
@@ -0,0 +1,37 @@
+function myAjax(obj,overTime){
+	const httpRequest = new XMLHttpRequest();
+	httpRequest.open( obj.method, obj.url, true )
+	const startTime = (new Date()).getTime();
+	const timmer = setInterval(()=>{
+			if( myAjax.timeIsOver(startTime,overTime||obj.overTime) ){
+				if(obj.timeOut){
+					// httpRequest.close() // 自动关闭网路请求
+					obj.timeOut(httpRequest) //如果超时调用timeout函数
+				}
+			}
+	}, 10);
+	
+	httpRequest.onreadystatechange = function(){
+		if(this.readyState!==4){ // 当网络请求完成到第四步
+			return
+		}
+		if(this.status===200){ // 并且状态为200 成功的时候
+			clearInterval(timmer);
+			if(obj.success){
+				obj.success(this.responseText) //调用成功的回调			
+			}
+
+		}
+		clearInterval(timmer);
+		if(obj.error){
+			obj.error(this.responseText) // 否则调用失败的
+		}
+	}
+	httpRequest.send(JSON.stringify(obj.data||{}));
+}
+
+
+myAjax.timeIsOver = (startTime, overTime)=>{
+	const currentTime = (new Date()).getTime()
+	return currentTime-startTime >= overTime // 当前时间减去 网路请求开始的时间 大于等于 超时时间 则判断为超时
+}
