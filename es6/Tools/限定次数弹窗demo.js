8a62537a5d27597d60eda1423250d6379b6b9a72
shanshan_pan <shanshan_pan@kingdee.com>
2018-11-12 09:53:24 +0800

    限定此处弹窗

--- es6/Tools/限定次数弹窗demo.js
+++ es6/Tools/限定次数弹窗demo.js
@@ -7,11 +7,11 @@ function is0_12(date) { // 判断一个字符串时间戳 是否在当天的 00 
     return _00 <= time && _12 > time // 传入的时间是否在 00-12 点之间 是为 true 否 为 false
 }
 
-function is12_24(date) { // 判断一个字符串时间戳 是否在当天的 13 - 24 点
-    let _12 = new Date().setHours(12) // 当天 13 点
+function is12_24(date) { // 判断一个字符串时间戳 是否在当天的 12 - 24 点
+    let _12 = new Date().setHours(12) // 当天 12 点
     let _24 = new Date().setHours(24) // 当天 24 点
     let time = new Date(Number(date)).getTime() // 传入的时间
-    return _12 <= time && _24 > time // 传入的时间是否在 13-24 点之间 是为 true 否 为 false
+    return _12 <= time && _24 > time // 传入的时间是否在 12-24 点之间 是为 true 否 为 false
 }
 
 function setCookie(name,value,days) { // setCookie 这个没什么好说的
@@ -36,8 +36,8 @@ function whenPageOpened(){ // 当页面被打开的时候
         if( !is0_12(lastPopUpedTime) ) { // 当天的 0-12 点 没有谈过窗
             popUp() // 弹窗
         }
-    } else { // 现在是 13-24 点
-        if( !is12_24(lastPopUpedTime) ) { // 当天的 13-24 点 没有谈过窗
+    } else { // 现在是 12-24 点
+        if( !is12_24(lastPopUpedTime) ) { // 当天的 12-24 点 没有谈过窗
             popUp() // 弹窗
         }
     }
