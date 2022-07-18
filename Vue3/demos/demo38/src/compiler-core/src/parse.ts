2f9a52e3031b8880a9cf92b6ed45746ef91a2b1d
PsChina <panshanzxcv.love@163.com>
2022-07-18 22:41:26 +0800

    实现解析插值逻辑

---
+++ Vue3/demos/demo38/src/compiler-core/src/parse.ts
@@ -0,0 +1,65 @@
+import { NodeTypes } from "./ast"
+
+interface ParserContext {
+    source:string
+}
+
+export function baseParse(content:string){
+    const context = createParserContext(content)
+    return createRoot(parseChildren(context))
+}
+
+function parseChildren(context:ParserContext){
+    const nodes:any[] = []
+    let node
+    if(context.source.startsWith('{{')){
+        node = parseInterplation(context)
+    }
+    nodes.push(node)
+
+
+    return nodes
+}
+
+function parseInterplation(context:ParserContext){
+
+    const openDelimiter = '{{'
+
+    const closeDelimiter = '}}'
+
+    const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+
+    advaceBy(context, openDelimiter.length)
+
+    const rawContentLength = closeIndex - openDelimiter.length
+
+    const rawContent = context.source.slice(0, rawContentLength)
+
+    const content = rawContent.trim()
+
+    advaceBy(context, rawContentLength + closeDelimiter.length)
+
+    return {
+        type: NodeTypes.INTERPOLATION,
+        content:{
+            type:NodeTypes.SIMPLE_EXPRETION,
+            content,
+        }
+    }
+}
+
+function advaceBy(context:ParserContext, length:number){
+    context.source = context.source.slice(length)
+}
+
+function createParserContext(content:string):ParserContext{
+    return {
+        source:content
+    }
+}
+
+function createRoot(children){
+    return {
+        children,
+    }
+}
